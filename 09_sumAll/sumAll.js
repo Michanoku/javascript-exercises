666f7052c5d92ed81e0f9c403073700ef1edb752
Michanoku <michael.werker@gmail.com>
2025-08-01 07:46:03 +0000

    Add sum

--- 09_sumAll/sumAll.js
+++ 09_sumAll/sumAll.js
@@ -1,11 +1,17 @@
 const sumAll = function(a, b) {
-    // Initialize the sum
-    let sum = a;
-    // Use the counter for iteration
-    for (i = a + 1; i < b + 1; i++) {
-        sum+= i;
+    // Check if both values are valid integers
+    if (!Number.isInteger(a) || !Number.isInteger(b) || a < 0 || b < 0 ) {
+        return "ERROR"
+    } else {
+        // Find start and end by comparing which value is bigger
+        const [start, end] = (a < b) ? [a, b] : [b, a];
+        // Set the initial sum to the starting value
+        let sum = start;
+        for (i = start + 1; i < end + 1; i++) {
+            sum+= i;
+        }
+        return sum;
     }
-    return sum;
 };
 
 // Do not edit below this line
