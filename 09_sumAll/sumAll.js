ecb329e9bcac5e95ac770589eeb2ee5d7b14ccd9
Michanoku <michael.werker@gmail.com>
2025-08-01 07:28:07 +0000

    Add basic calculation

--- 09_sumAll/sumAll.js
+++ 09_sumAll/sumAll.js
@@ -1,5 +1,11 @@
-const sumAll = function() {
-
+const sumAll = function(a, b) {
+    // Initialize the sum
+    let sum = a;
+    // Use the counter for iteration
+    for (i = a + 1; i < b + 1; i++) {
+        sum+= i;
+    }
+    return sum;
 };
 
 // Do not edit below this line
