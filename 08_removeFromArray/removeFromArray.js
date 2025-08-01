70dc18979ad1cd5e7d5d18d376b1fb0d68c32ead
Michanoku <michael.werker@gmail.com>
2025-08-01 07:20:28 +0000

    Remove from array.

--- 08_removeFromArray/removeFromArray.js
+++ 08_removeFromArray/removeFromArray.js
@@ -1,4 +1,12 @@
 const removeFromArray = function() {
+    // Get First Argument, which is the Array
+    let array = arguments[0]
+    // Loop over the rest of the arguments, which need to be removed from the array
+    for (i = 1; i < arguments.length; i++) {
+        // Filter the argument out and overwrite the array
+        array = array.filter(item => item !== arguments[i]);
+    }
+    return array;
 };
 
 // Do not edit below this line
