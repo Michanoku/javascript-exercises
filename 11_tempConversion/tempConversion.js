4cbb6a9d7dd8b4238f3e6826feb82ccaa4acb070
Michanoku <michael.werker@gmail.com>
2025-08-01 08:01:33 +0000

    Convert temperatures

--- 11_tempConversion/tempConversion.js
+++ 11_tempConversion/tempConversion.js
@@ -1,7 +1,15 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(temp) {
+  // Convert to Celsius
+  const result = (temp - 32) * (5/9); 
+  // Round and return
+  return Math.round(result * 10) / 10;
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(temp) {
+  // Convert to Fahrenheit
+  result = temp * (9/5) + 32; 
+  // Round and return
+  return Math.round(result * 10) / 10;
 };
 
 // Do not edit below this line
