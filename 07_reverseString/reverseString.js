94c261b291dbea51315ef62d342aefa47310fae3
Michanoku <michael.werker@gmail.com>
2025-08-01 06:58:17 +0000

    Reverse the string

--- 07_reverseString/reverseString.js
+++ 07_reverseString/reverseString.js
@@ -1,5 +1,10 @@
-const reverseString = function() {
-
+const reverseString = function(str) {
+    // Split the string into the array
+    const stringArray = str.split("");
+    // Reverse the array
+    stringArray.reverse();
+    // Join the array into the string
+    return stringArray.join("");
 };
 
 // Do not edit below this line
