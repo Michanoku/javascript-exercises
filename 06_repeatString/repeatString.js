109c838696c842bc6c0c78b1d825f6e7e28166a6
Michanoku <michael.werker@gmail.com>
2025-08-01 06:50:29 +0000

    Solve 06 while forgetting git

--- 06_repeatString/repeatString.js
+++ 06_repeatString/repeatString.js
@@ -1,5 +1,12 @@
-const repeatString = function() {
-
+const repeatString = function(str, count) {
+    if (count < 0) {
+        return "ERROR";
+    }
+    const stringArray = [];
+    for (i = 0; i < count; i++) {
+        stringArray.push(str);
+    }
+    return stringArray.join("");
 };
 
 // Do not edit below this line
