c272d40e9f3a9f59600d9077350cde1680db2e0a
Michanoku <michael.werker@gmail.com>
2025-08-05 02:21:01 +0000

    Add age calculation

--- 16_findTheOldest/findTheOldest.js
+++ 16_findTheOldest/findTheOldest.js
@@ -1,7 +1,16 @@
 const findTheOldest = function(people) {
     people.sort((personA, personB) => {
-        return (personA.yearOfDeath - personA.yearOfBirth) - (personB.yearOfDeath - personB.yearOfBirth);
+        function age(person) {
+            if (!"yearOfDeath" in person) {
+                const currentYear = new Date().getFullYear();
+                return currentYear - person.yearOfBirth;
+            } else {
+                return person.yearOfDeath - person.yearOfBirth;
+            }
+        }
+        return ((age(personB) - age(personA)));
     });
+    return people[0]
 };
 
 // Do not edit below this line
