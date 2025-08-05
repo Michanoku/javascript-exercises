1ac38e009cd05e6863842a4ce6f3e9d239bfb5e9
Michanoku <michael.werker@gmail.com>
2025-08-05 01:23:46 +0000

    Add palindrome check first version

--- 13_palindromes/palindromes.js
+++ 13_palindromes/palindromes.js
@@ -1,5 +1,16 @@
-const palindromes = function () {
-
+const palindromes = function (string) {
+    // Split string to array
+    let stringArr = string.split("");
+    // Filter out all non alphabet characters after converting to lowercase
+    const lowerArr = stringArr.filter(character => {
+        const lowerCase = character.toLowerCase();
+        const code = lowerCase.charCodeAt(0);
+        return (code >= 97 && code <= 122);
+    });
+    // Create the reverse array
+    const reverseArr = lowerArr.reverse();
+    // Return true if arrays are the same 
+    return lowerArr.join("") === reverseArr.join("");
 };
 
 // Do not edit below this line
