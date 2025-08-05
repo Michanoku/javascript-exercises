d9360d47da34dc9ec1dcfe1143bbc8b847359ae2
Michanoku <michael.werker@gmail.com>
2025-08-05 01:48:16 +0000

    14 - Add fibonacci logic

--- 14_fibonacci/fibonacci.js
+++ 14_fibonacci/fibonacci.js
@@ -1,5 +1,13 @@
-const fibonacci = function() {
-
+const fibonacci = function(parameter) {
+    const number = parseInt(parameter);
+    if (number < 0 ) {
+        return "OOPS";
+    }
+    if (number <= 1) {
+        return number;
+    } else { 
+        return fibonacci(number - 1) + fibonacci(number - 2);
+    }
 };
 
 // Do not edit below this line
